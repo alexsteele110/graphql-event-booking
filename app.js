20a86d58eeba96307a99359b2b01782ce03466d3
Alex Steele <alexsteele110@gmail.com>
2019-01-29 17:50:18 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolvers = require('./graphql/resolvers');
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@graphql-event-booking-go8yh.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,105 @@
+const bcrypt = require('bcryptjs');
+
+const Event = require('../../models/event');
+const User = require('../../models/user');
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event.creator),
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async userId => {
+  try {
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return {
+          ...event._doc,
+          _id: event._doc._id.toString(),
+          date: new Date(event._doc.date).toISOString(),
+          creator: user.bind(this, event._doc.creator),
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async ({ eventInput }) => {
+    const { title, description, price, date } = eventInput;
+
+    const event = new Event({
+      title,
+      description,
+      price,
+      date: new Date(date),
+      creator: '5c50d2897c04e22bacbc88b5',
+    });
+    let createdEvent;
+
+    try {
+      const result = await event.save();
+      createdEvent = {
+        ...result._doc,
+        _id: event._doc._id.toString(),
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, result._doc.creator),
+      };
+      const creator = await User.findById('5c50d2897c04e22bacbc88b5');
+
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  },
+  createUser: async ({ userInput }) => {
+    const { email, password } = userInput;
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        throw new Error('User exists already.');
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const user = new User({
+        email,
+        password: hashedPassword,
+      });
+
+      const result = await user.save();
+      return { ...result._doc, password: null, _id: result.id };
+    } catch (err) {
+      throw err;
+    }
+  },
+};
